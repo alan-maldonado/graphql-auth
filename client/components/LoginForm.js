6b6a7f30ee0ae201a06b25cdfa505ca0522a181a
Alan Maldonado <alan.maldonado@gmail.com>
2021-11-06 17:14:27 -0600

    ✨ Login client

---
+++ client/components/LoginForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import mutation from '../mutations/Login';
+import { graphql } from 'react-apollo';
+import AuthForm from './AuthForm';
+
+class LoginForm extends Component {
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: { email, password }
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h3>Login</h3>
+        <AuthForm onSubmit={this.onSubmit.bind(this)} />
+      </div>
+    );
+  }
+}
+
+export default graphql(mutation)(LoginForm);
